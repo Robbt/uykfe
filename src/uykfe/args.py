f68b7bcb52629249af08c31985760318208909af
andrew cooke <andrew@acooke.org>
2011-03-26 14:21:40 -0300

    weighting

---
+++ src/uykfe/args.py
@@ -0,0 +1,23 @@
+
+from argparse import ArgumentError, ArgumentParser
+
+
+def non_zero_int(value):
+    try:
+        nzi = int(value)
+        if nzi < 0:
+            raise ArgumentError('{0} is negative.'.format(value))
+        else:
+            return nzi
+    except:
+        raise ArgumentError('{0} is not an integer.'.format(value))
+
+
+def build_weighted_parser(description):
+    parser = ArgumentParser(description=description)
+    parser.add_argument('-a', '--artist', help='starting artist')
+    parser.add_argument('-t', '--track', help='starting track')
+    parser.add_argument('-d', '--depth', default=0, type=non_zero_int, help='depth for delayed weighting')
+    parser.add_argument('-x', '--localexp', default=1, type=int, help='exponent for local weight')
+    parser.add_argument('-y', '--depthexp', default=1, type=int, help='exponent for depth weight')
+    return parser
--- src/uykfe/playlist.py
+++ src/uykfe/playlist.py
@@ -1,39 +1,16 @@
 
 from itertools import islice
 from logging import getLogger, basicConfig, DEBUG, INFO
-from argparse import ArgumentParser, ArgumentError
 from random import choice
 
 from uykfe.support.db import open_db, LocalArtist, LocalTrack
 from uykfe.sequence.base import sequence
 from uykfe.sequence.static import StaticState
 from uykfe.sequence.weighted.weighted import WeightedControl
+from uykfe.args import build_weighted_parser
 
 
 LOG = getLogger(__name__)
-
-
-def non_zero_int(value):
-    try:
-        nzi = int(value)
-        if nzi < 0:
-            raise ArgumentError('{0} is negative.'.format(value))
-        else:
-            return nzi
-    except:
-        raise ArgumentError('{0} is not an integer.'.format(value))
-
-
-def build_parser():
-    parser = ArgumentParser(description='Print a playlist to stdout')
-    parser.add_argument('count', metavar='N', type=int,
-                        help='the number of entries')
-    parser.add_argument('-a', '--artist', help='starting artist')
-    parser.add_argument('-t', '--track', help='starting track')
-    parser.add_argument('-d', '--depth', default=0, type=non_zero_int, help='depth for delayed weighting')
-    parser.add_argument('-x', '--localexp', default=1, type=int, help='exponent for local weight')
-    parser.add_argument('-y', '--depthexp', default=1, type=int, help='exponent for depth weight')
-    return parser
 
 
 def find_track(session, artist, title):
@@ -55,7 +32,8 @@ def find_track(session, artist, title):
 
 if __name__ == '__main__':
     basicConfig(level=INFO)
-    parser = build_parser()
+    parser = build_weighted_parser('Print a playlist to stdout')
+    parser.add_argument('count', metavar='N', type=int, help='the number of entries')
     args = parser.parse_args()
     session = open_db()()
     track = find_track(session, args.artist, args.track)
