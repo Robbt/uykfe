8d2b71f26e635a784b8d992ba843d6ac7532aa2b
andrew cooke <andrew@acooke.org>
2011-03-26 14:21:40 -0300

    weighting

--- src/uykfe/play.py
+++ src/uykfe/play.py
@@ -5,7 +5,8 @@ from uykfe.support.db import open_db
 from uykfe.support.squeeze import SqueezeServer
 from uykfe.sequence.base import sequence
 from uykfe.sequence.squeeze import SqueezeState
-from uykfe.sequence.simple.simple import SimpleControl
+from uykfe.args import build_weighted_parser
+from uykfe.sequence.weighted.weighted import WeightedControl
 
 
 LOG = getLogger(__name__)
@@ -13,9 +14,11 @@ LOG = getLogger(__name__)
 
 if __name__ == '__main__':
     basicConfig(level=INFO)
+    parser = build_weighted_parser('Add items to SqueezeCenter playlist as needed')
+    args = parser.parse_args()
     squeeze = SqueezeServer()
     state = SqueezeState(open_db()(), squeeze)
-    control = SimpleControl()
+    control = WeightedControl(state, args.localexp, args.depth, args.depthexp)
     state.wait()
     for track in sequence(state, control):
         squeeze.playlist_add(track.url)
