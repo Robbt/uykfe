ce3d3a34540d9f2cc16a0456be9fc60f0d730ffb
andrew cooke <andrew@acooke.org>
2011-03-26 16:52:01 -0300

    config from command line

--- src/uykfe/play.py
+++ src/uykfe/play.py
@@ -15,9 +15,10 @@ LOG = getLogger(__name__)
 if __name__ == '__main__':
     basicConfig(level=INFO)
     parser = build_weighted_parser('Add items to SqueezeCenter playlist as needed')
+    parser.add_argument('-c', '--config', default='.uykferc', help='config file')
     args = parser.parse_args()
     squeeze = SqueezeServer(name=args.config)
-    state = SqueezeState(open_db(name=args.config)(), squeeze)
+    state = SqueezeState(open_db()(), squeeze)
     control = WeightedControl(state, args.localexp, args.depth, args.depthexp)
     state.wait()
     for track in sequence(state, control):
