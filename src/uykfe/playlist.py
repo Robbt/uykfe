dc32ffec155b28a9d26ff8dfaebe90da5f22794f
andrew cooke <andrew@acooke.org>
2011-03-26 14:10:29 -0300

    weighting

--- src/uykfe/playlist.py
+++ src/uykfe/playlist.py
@@ -54,7 +54,7 @@ def find_track(session, artist, title):
 
 
 if __name__ == '__main__':
-    basicConfig(level=DEBUG)
+    basicConfig(level=INFO)
     parser = build_parser()
     args = parser.parse_args()
     session = open_db()()
