d9901af89723a96263539351b390064bbf3b74a5
andrew cooke <andrew@acooke.org>
2011-03-30 20:16:40 -0300

    use stagger egg

--- setup-env.sh
+++ setup-env.sh
@@ -2,9 +2,7 @@
 
 virtualenv3 -p /usr/local/bin/python3.2 --no-site-packages env
 . env/bin/activate
-cd stagger-read-only
-python setup.py build
-python setup.py install
 easy_install sqlalchemy
+easy_install stagger
 easy_install altgraph
 easy_install networkx
